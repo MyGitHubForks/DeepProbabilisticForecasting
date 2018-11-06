6e569db9171a151fd0c9eb8387c6e1f588387516
Daniel Zeiberg <zeiberg.d@husky.neu.edu>
2018-11-06 08:55:00 -0500

    fixed grid search params selection, changed to random search

--- gridSearchOptimize.py
+++ gridSearchOptimize.py
@@ -2,19 +2,30 @@ from train import train
 from joblib import Parallel, delayed
 import os
 
-params = {"h_dim": np.linspace(16, 512, 20),
-	"z_dim": np.linspace(16, 512,20),
-	"batch_size": np.linspace(1, 256,20),
-	"n_layers": [1,2,3],
-	"initial_lr": np.linspace(.00001, .01, 10),
-	"weight_decay": np.linspace(.000005, .5, 10),
-	"scheduling_start": np.linspace(.3, .8, 10),
-	"scheduling_end": np.linspace(0.0, 0.3, 10)}
+params = {"h_dim": (16, 513),
+	"z_dim": (16, 513),
+	"batch_size": (1, 257)
+	"n_layers": (1,4),
+	"initial_lr": (-5, -1),
+	"weight_decay": (-6, -1),
+	"scheduling_start": (.3, .8),
+	"scheduling_end": (0.0, 0.3)}
 
 def getParams():
 	p = {}
-	for key, vals in params.items():
-		p[key] = np.random.choice(vals)
+	for key in ["h_dim", "z_dim", "batch_size", "n_layers"]:
+		vals = params[key]
+		p[key] = np.random.randint(vals[0], vals[1])
+
+	for key in ["weight_decay", "initial_lr"]:
+		vals = params[key]
+		possib = np.logspace(vals[0], vals[1], num=5)
+		p[key] = np.random.choice(possib)
+
+	for key in ["scheduling_end", "scheduling_start"]:
+		vals = params[key]
+		possib = np.linspace(vals[0], vals[1], num=5)
+		p[key] = np.random.choice(possib)
 	return p
 
 def runExperiment():
@@ -27,11 +38,11 @@ def getSaveFile():
 		os.mkdir("./save/")
 	if not os.path.isdir("./save/gridSearch/"):
 		os.mkdir("./save/gridSearch/")
-    while os.path.isfile(saveFile):
-        numStart = saveDir.rfind("_")+1
-        numEnd = saveDir.rfind(".")
-        saveFile = saveFile[:numStart] + str(int(saveFile[numStart:numEnd])+1) + ".txt"
-    return saveFile
+	while os.path.isfile(saveFile):
+		numStart = saveDir.rfind("_")+1
+		numEnd = saveDir.rfind(".")
+		saveFile = saveFile[:numStart] + str(int(saveFile[numStart:numEnd])+1) + ".txt"
+	return saveFile
 
 def main():
 	saveFile = getSaveFile()
