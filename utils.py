be5842a6e93deb04df82cb05f2c0f533bb17e144
Dzeiberg <dzeiberg@me.com>
2018-11-09 09:45:39 -0500

    legend

--- utils.py
+++ utils.py
@@ -65,6 +65,7 @@ def plotTrainValCurve(trainLosses, valLosses, model_description, lossDescription
     ax1.plot(np.arange(1, len(trainLosses)+1)*args.plot_every, trainLosses, "r--", label="train reconstruction loss")
     ax1.plot(np.arange(1, len(valLosses)+1)*args.plot_every, valLosses, color="red", label="validation reconstruction loss")
     ax1.legend(loc="upper left")
+    ax1.grid()
     if trainKLDLosses:
         ax2 = ax1.twinx()
         ax2.set_ylabel("KLD Loss", color="b")
@@ -72,6 +73,7 @@ def plotTrainValCurve(trainLosses, valLosses, model_description, lossDescription
         ax2.plot(np.arange(1, len(trainKLDLosses)+1)*args.plot_every, trainKLDLosses, "b--", label="train KLD loss")
         ax2.plot(np.arange(1, len(valKLDLosses)+1)*args.plot_every, valKLDLosses, color="blue", label="val KLD loss")
         ax2.legend(loc="upper right")
+        ax2.grid()
     plt.title("Losses for {}".format(model_description))
     plt.savefig(args.save_dir+"train_val_loss_plot.png")
 
