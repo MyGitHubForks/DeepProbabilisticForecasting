087885ca8d02fbe87176b09d1b66ef032a4d946e
Daniel Zeiberg <zeiberg.d@husky.neu.edu>
2018-11-20 22:54:30 -0500

    reshape data in preprocessing

--- utils.py
+++ utils.py
@@ -29,8 +29,6 @@ def load_human_dataset(dataset_dir, batch_size, down_sample=None, load_test=Fals
     for category in cats:
         print(category)
         f = h5py.File(os.path.join(dataset_dir, category+".h5"))
-        f["input2d"] = f["input2d"].reshape((f['input2d'].shape[:2]+[-1]))
-        f["target2d"] = f["target2d"].reshape((f['target2d'].shape[:2]+[-1]))
         if down_sample:
             nRows = f["input2d"].shape[0]
             down_sampled_rows = np.random.choice(range(nRows), size=np.ceil(nRows * down_sample).astype(int),
