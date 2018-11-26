4e4206968afa79a65516826770b91551da9d195b
daniel zeiberg <zeiberg.d@husky.neu.edu>
2018-11-26 14:14:59 +0000

    print model and dataset at beginning of gridsearch output

--- gridSearchOptimize.py
+++ gridSearchOptimize.py
@@ -48,17 +48,19 @@ def main():
 	args = parser.parse_args()
 	tries = args.tries
 	saveDirs = [getSaveDir() for i in range(tries)]
-	results = Parallel(n_jobs=2)(delayed(runExperiment)(args, saveDirs[i]) for i in range(tries))
+	results = Parallel(n_jobs=4)(delayed(runExperiment)(args, saveDirs[i]) for i in range(tries))
 	# trainReconLosses, trainKLDLosses, valReconLosses, valKLDLosses, args.save_dir
 	results = sorted(results, key=lambda x: x[2])
 	saveFile = getSaveFile()
 	if args.model == "rnn":
 		with open(saveFile, "w+") as f:
+			f.write("Dataset: {}, Model: {}\n".format(args.dataset, args.model))
 			f.write("Save Directory\t\tTrain Loss\t\tValidation Loss\n")
 			for tup in results:
 				f.write("{}\t\t{:.3f}\t\t{:.3f}\n".format(tup[4], tup[0], tup[2]))
-	elif args.model == "vrnn":
+	elif args.model == "vrnn" or args.model=="sketch-rnn":
 		with open(saveFile, "w+") as f:
+			f.write("Dataset: {}, Model: {}".format(args.dataset, args.model))
 			f.write("Save Directory\t\tTrain Recon Loss\tTrain KLD Loss\tValidation Recon Loss\tValidation KLD Loss\n")
 			for tup in results:
 				f.write("{}\t\t{:.3f}\t\t{:.3f}\t\t{:.3f}\t\t\t{:.3f}\n".format(tup[4], tup[0],tup[1],tup[2], tup[3]))
