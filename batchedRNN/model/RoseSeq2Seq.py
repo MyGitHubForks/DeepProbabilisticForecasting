4848b8422c498157dc91bd0bd8c56c64ddcdcd37
Daniel Zeiberg <zeiberg.d@husky.neu.edu>
2018-11-30 12:57:48 -0500

    bidirectional encoder

--- batchedRNN/model/RoseSeq2Seq.py
+++ batchedRNN/model/RoseSeq2Seq.py
@@ -9,24 +9,29 @@ import numpy as np
 from memory_profiler import profile
 
 class EncoderRNN(nn.Module):
-    def __init__(self, input_size, hidden_size, n_layers=2, args=None):
+    def __init__(self, input_size, hidden_size, n_layers=2, bidirectional=True, args=None):
         super(EncoderRNN, self).__init__()
         self.args = args
         self.input_size = input_size
         self.n_layers = n_layers
         self.hidden_size = hidden_size
         self.embedding = nn.Linear(input_size, hidden_size)
-        self.gru = nn.GRU(hidden_size * self.args.channels, hidden_size, n_layers, dropout=self.args.dropout)
-
+        self.gru = nn.GRU(hidden_size * self.args.channels, hidden_size, n_layers, dropout=self.args.dropout, bidirectional=bidirectional)
+        self.input_dropout = nn.Dropout(p=self.args.dropout)
     def forward(self, input, hidden):
         embedded = self.embedding(input)
+        embedded = self.input_dropout(embedded)
         embedded = torch.unsqueeze(embedded, 0)
         embedded = embedded.view(1, self.args.batch_size, -1)
         output, hidden = self.gru(embedded, hidden)
         return output, hidden
 
     def initHidden(self):
-        result = Variable(torch.zeros(self.n_layers, self.args.batch_size, self.hidden_size))
+        if self.args.bidirectionalEncoder:
+            directions = 2
+        else:
+            directions = 1
+        result = Variable(torch.zeros(self.n_layers * directions, self.args.batch_size, self.hidden_size))
         if self.args.cuda:
             return result.cuda()
         else:
@@ -41,7 +46,7 @@ class DecoderRNN(nn.Module):
         self.hidden_size = hidden_size
 
         self.embedding = nn.Linear(output_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size, n_layers, dropout=self.args.dropout)
+        self.gru = nn.GRU(hidden_size, 2 * hidden_size, n_layers, dropout=self.args.dropout)
         self.out = nn.Linear(hidden_size, output_size)
 
     def forward(self, input, hidden):
@@ -53,25 +58,25 @@ class DecoderRNN(nn.Module):
         #print("decoder output", output[10,31])
         return output, hidden
 
-    def initHidden(self):
-        result = Variable(torch.zeros(self.n_layers, self.args.batch_size, self.hidden_size))
-        if self.args.cuda:
-            return result.cuda()
-        else:
-            return result
-
 class Seq2Seq(nn.Module):
     def __init__(self, args):
         super(Seq2Seq, self).__init__()
         self.args = args
 
-        self.enc = EncoderRNN(self.args.x_dim, self.args.h_dim, n_layers=self.args.n_layers, args=args)
+        self.enc = EncoderRNN(self.args.x_dim, self.args.h_dim, n_layers=self.args.n_layers, bidirectional=args.bidirectionalEncoder, args=args)
 
         self.dec = DecoderRNN(self.args.h_dim, self.args.x_dim, n_layers=self.args.n_layers, args=args)
 
         self.use_schedule_sampling = args.use_schedule_sampling
         self.scheduling_start = args.scheduling_start
         self.scheduling_end = args.scheduling_end
+
+    def _cat_directions(self, h):
+        """ If the encoder is bidirectional, do the following transformation.
+            (#directions * #layers, #batch, hidden_size) -> (#layers, #batch, #directions * hidden_size)
+        """
+        h = torch.cat([h[0:h.size(0):2], h[1:h.size(0):2]], 2)
+        return h
 
     def parameters(self):
         return list(self.enc.parameters()) + list(self.dec.parameters())
@@ -88,8 +93,10 @@ class Seq2Seq(nn.Module):
         for t in range(self.args.sequence_len):
             encoder_output, encoder_hidden = self.enc(x[t].squeeze(), encoder_hidden)
             hs += [encoder_output]
-
-        decoder_hidden = encoder_hidden
+        if args.bidirectionalEncoder:
+            decoder_hidden = self._cat_directions(encoder_hidden)
+        else:
+            decoder_hidden = encoder_hidden
         # Prepare for Decoder
         inp = Variable(torch.zeros(self.args.batch_size, self.args.x_dim))
         if self.args.cuda:
