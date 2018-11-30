a63d8f310f5cfe4a8acac7e47e1d1172f6b7ac03
Daniel Zeiberg <zeiberg.d@husky.neu.edu>
2018-11-29 19:01:16 -0500

    transpose data in DataLoaderWithTime

--- batchedRNN/model/Data.py
+++ batchedRNN/model/Data.py
@@ -10,19 +10,14 @@ class DataLoader(object):
             num_padding = (batch_size - (len(xs) % batch_size)) % batch_size
             x_padding = np.repeat(xs[-1:], num_padding, axis=0)
             y_padding = np.repeat(ys[-1:], num_padding, axis=0)
-            # tx_padding = np.repeat(tx[-1:], num_padding, axis=0)
-            # ty_padding = np.repeat(ty[-1:], num_padding, axis=0)
             xs = np.concatenate([xs, x_padding], axis=0)
             ys = np.concatenate([ys, y_padding], axis=0)
-            # tx = np.concatenate([tx, tx_padding], axis=0)
-            # ty = np.concatenate([ty, ty_padding], axis=0)
         self.size = len(xs)
         self.num_batch = int(self.size // self.batch_size)
         self.xs = xs
         self.ys = ys
         if shuffle:
             self.shuffle()
-        
 
     def get_iterator(self):
         self.current_ind = 0
@@ -73,10 +68,10 @@ class DataLoaderWithTime(object):
             while self.current_ind < self.num_batch:
                 start_ind = self.batch_size * self.current_ind
                 end_ind = min(self.size, self.batch_size * (self.current_ind + 1))
-                x_i = self.xs[start_ind: end_ind, ...]
-                y_i = self.ys[start_ind: end_ind, ...]
-                tx_i = self.tx[start_ind: end_ind, ...]
-                ty_i = self.ty[start_ind: end_ind, ...]
+                x_i = np.transpose(self.xs[start_ind: end_ind, ...], (1,0,3,2))
+                y_i = np.transpose(self.ys[start_ind: end_ind, :,:,0], (1,0,2))
+                tx_i = np.transpose(self.tx[start_ind: end_ind, ...], (1,0))
+                ty_i = np.transpose(self.ty[start_ind: end_ind, ...], (1,0))
                 yield (x_i, y_i, tx_i, ty_i)
                 self.current_ind += 1
 
