e50c12cce6c80c164d0cd93e4e99917e2201e566
Daniel Zeiberg <zeiberg.d@husky.neu.edu>
2018-12-10 10:46:10 -0500

    import numpy for debugging

--- batchedRNN/train.py
+++ batchedRNN/train.py
@@ -2,6 +2,7 @@ from utils import *
 from model.RoseSeq2Seq import Seq2Seq
 from model.SketchRNN import SketchRNN
 from torch.optim.lr_scheduler import MultiStepLR
+import numpy as np
 
 def main():
     # Set Additional Args
